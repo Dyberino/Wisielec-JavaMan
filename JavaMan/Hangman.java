3ea5a2b256d5eb5baa9c428463d86d64abbf304b
Dyberino <dyba.piotrek@gmail.com>
2021-12-30 16:10:53 +0100

    Piotrek 30.12

--- JavaMan/Hangman.java
+++ JavaMan/Hangman.java
@@ -20,24 +20,20 @@ public class Hangman{
         }
 
         Random rand = new Random();
-        String word = words.get(rand.nextInt(words.size()));
+       String word = words.get(rand.nextInt(words.size()));
 
-        // System.out.println(word);
+        System.out.println(word);
         List<Character> guess = new ArrayList<>();
         printWord(word,guess);
 
-        System.out.println("Podaj literę: ");
-        String letterGuess = keyboard.nextLine();
-            try {
-                if (letterGuess.length() > 1)
-                    throw new Exception("Wpisz tylko 1 literę");
-                    System.out.println("Podaj literę: ");
-                    letterGuess = keyboard.nextLine();
-            } catch (Exception e) {
-                System.out.println("Blad" + e);
-            }
+        Boolean loop = true;
+        while(loop){
+            playerGuess(keyboard, word, guess);
+            
         }
+
         
+    }
     private static void printWord(String word,List<Character>guess){
 
         for (int i=0;i<word.length();i++){
@@ -48,5 +44,22 @@ public class Hangman{
                 System.out.print("_ ");
             }
     }
+    }
+    private static void playerGuess(Scanner keyboard, String word,List<Character>guess){
+        System.out.println("Podaj literę: ");
+        String letterGuess = keyboard.nextLine();
+        // while(letterGuess.length()>1){
+        //     try {
+        //         System.out.println("Podaj literę: ");
+        //         letterGuess = keyboard.nextLine();
+        //         if (letterGuess.length() > 1)
+        //         throw new Exception("Wpisz tylko 1 literę");
+                    
+        //     } catch (Exception e) {
+        //         System.out.println(e.getMessage());
+        //     }
+        // }
+        guess.add(letterGuess.charAt(0));
+        printWord(word, guess);
     }
 }
