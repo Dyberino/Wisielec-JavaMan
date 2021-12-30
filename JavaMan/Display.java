9a5e0912094ddb804285df979b44a6698c287fe3
KonradChecinski <konradchecinski2012@gmail.com>
2021-12-30 16:08:20 +0100

    Konrad

--- JavaMan/Display.java
+++ JavaMan/Display.java
@@ -1,55 +1,74 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.*;
+import java.util.Locale;
+
 import javax.swing.CellRendererPane;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Display extends JPanel {
+public class Display extends JPanel implements ActionListener{
 
-        private static final int N = 8;
-        private static final String s = "<html>";
-        private JLabel renderer = new JLabel(s);
-        private CellRendererPane crp = new CellRendererPane();
-        private Dimension dim;
+        private final int windowWidth = 900;
+        private final int windowHeight = 700;
+
+        private final int textInputWidth = 100;
+        private final int textInputHeight = 100;
+
+
+        JTextField textInput = new JTextField("");
         
        
     
         public Display() {
             this.setBackground(Color.white);
-            dim = renderer.getPreferredSize();
-            this.add(crp);
-            
+            // dim = renderer.getPreferredSize();
+            // this.add(crp);
         }
     
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            for (int i = 0; i < 1; i++) {
-                renderer.setForeground(Color.getHSBColor((float) i / N, 1, 1));
-                crp.paintComponent(g, renderer, this,
-                    i * dim.width, i * dim.height, dim.width, dim.height);
-            }
+
+        public void actionPerformed(ActionEvent e) { 
+            System.out.println(textInput.getText());
+            textInput.setText("");
+        }
+
+        public void actionPerformed(KeyEvent e) { 
+            System.out.println(textInput.getText());
+            textInput.setText("");
         }
     
         private void display() {
-            JTextField textInput = new JTextField("TextInput");
-            textInput.setBounds(50,100, 200,30);
+            Locale locale = new Locale("pl", "PL");
+            Font font = new Font("Arial", Font.BOLD, 60);
+            
+            textInput.setBounds(windowWidth/2-textInputWidth/2,windowHeight-(int)(0.25*(windowHeight)), textInputWidth, textInputHeight);
+            textInput.setLocale(locale);
+            textInput.setColumns(2);
+            textInput.setFont(font);
+            textInput.setBackground(new Color(255, 0, 0));
+            textInput.setHorizontalAlignment(JTextField.CENTER);
+
+
+            textInput.addActionListener(this);
 
 
             JFrame f = new JFrame("Wisielec");
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.setIconImage(image);
-            f.add(this);
-            f.add(textInput);
+            ImageIcon img = new ImageIcon("JavaMan/icon.png");
+            f.setIconImage(img.getImage());
             f.pack();
-            f.setSize(700, 700);
+            f.setSize(windowWidth, windowHeight);
             f.setResizable(false);
             f.setLocationRelativeTo(null);
+
+            f.add(textInput);
+            f.setLayout(null);
             f.setVisible(true);
         }
     
