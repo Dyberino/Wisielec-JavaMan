53e8498ec28b9bfb7fd9b535b8b6256e073fe321
Piotr Dyba <dyba.piotrek@gmail.com>
2022-01-02 21:27:48 +0100

    Piotrek 02.01.22

--- JavaMan/Hangman.java
+++ JavaMan/Hangman.java
@@ -6,61 +6,101 @@ import java.io.FileNotFoundException;
 import java.text.Collator;
 import java.util.Scanner;
 import java.util.Locale;
-public class Hangman{
-    public static void main(String[] args)throws FileNotFoundException{
-        Locale locale = new Locale("pl","PL");
-        Scanner in = new Scanner(new File("JavaMan/WORDS.txt"),"UTF-8");
+
+public class Hangman {
+    public static void main(String[] args) throws FileNotFoundException {
+        Locale locale = new Locale("pl", "PL");
+        Scanner in = new Scanner(new File("JavaMan/WORDS.txt"), "UTF-8");
         Collator col = Collator.getInstance(locale);
         col.setStrength(Collator.PRIMARY);
-        Scanner keyboard = new Scanner(System.in); 
+        Scanner keyboard = new Scanner(System.in);
 
         List<String> words = new ArrayList<>();
-        while (in.hasNextLine()){
+        while (in.hasNextLine()) {
             words.add(in.nextLine());
         }
 
         Random rand = new Random();
-       String word = words.get(rand.nextInt(words.size()));
+        String word = words.get(rand.nextInt(words.size()));
 
         System.out.println(word);
         List<Character> guess = new ArrayList<>();
-        printWord(word,guess);
+        printWord(word, guess);
 
         Boolean loop = true;
-        while(loop){
-            playerGuess(keyboard, word, guess);
+        int errors = 0;
+        while (loop) {
+            switch (errors) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+
+            }
+
+            if (errors >= 8) {
+                System.out.println("Lose");
+                break;
+            }
+
+            printWord(word, guess);
+            if (!playerGuess(keyboard, word, guess)) {
+                errors++;
+                System.out.println("Frajer jestes i dzban");
+            }
+
+            if (printWord(word, guess)) {
+                System.out.println("Win");
+                break;
+            }
 
         }
 
-        
     }
-    private static void printWord(String word,List<Character>guess){
 
-        for (int i=0;i<word.length();i++){
-            if(guess.contains(word.charAt(i))){
+    private static boolean printWord(String word, List<Character> guess) {
+        int counter = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (guess.contains(word.charAt(i))) {
                 System.out.print(word.charAt(i));
-            }
-            else{
+                counter++;
+            } else {
                 System.out.print("_ ");
             }
+        }
+        return (word.length() == counter);
     }
-    }
-    private static void playerGuess(Scanner keyboard, String word,List<Character>guess){
+
+    private static boolean playerGuess(Scanner keyboard, String word, List<Character> guess) {
         System.out.println("Podaj literę: ");
         String letterGuess = keyboard.nextLine();
         // while(letterGuess.length()>1){
-        //     try {
-        //         System.out.println("Podaj literę: ");
-        //         letterGuess = keyboard.nextLine();
-        //         if (letterGuess.length() > 1)
-        //         throw new Exception("Wpisz tylko 1 literę");
-                    
-        //     } catch (Exception e) {
-        //         System.out.println(e.getMessage());
-        //     }
+        // try {
+        // System.out.println("Podaj literę: ");
+        // letterGuess = keyboard.nextLine();
+        // if (letterGuess.length() > 1)
+        // throw new Exception("Wpisz tylko 1 literę");
+
+        // } catch (Exception e) {
+        // System.out.println(e.getMessage());
         // }
-        // 
+        // }
+        //
         guess.add(letterGuess.charAt(0));
-        printWord(word, guess);
+
+        return word.contains(letterGuess);
     }
 }
